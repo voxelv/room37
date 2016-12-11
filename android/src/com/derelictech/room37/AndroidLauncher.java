59343c9c70a8ecc7d95c0be4ec0ee50e6e8bcba8
Tim Slippy <Tim Slippy>
2016-12-10 17:53:29 -0800

    initial commit

---
+++ android/src/com/derelictech/room37/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.derelictech.room37;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.derelictech.room37.Room37;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Room37(), config);
+	}
+}
